e8896cd38aac054c701a1a3865f37f487fe7611b
sga1d0s <galdos.sergio@outlook.com>
2024-11-26 22:08:12 +0100

    [dev][array] exer 8 and 9 done

---
+++ arrays/ejercicio8.js
@@ -0,0 +1,22 @@
+// 8. Declarar una función en la que se pase el array siguiente como argumento. 
+// A continuación extraiga el elemento mayor y el menor del array, 
+// y devuelva la diferencia entre ambos valores. 
+// Ejecutar la función y mostrar el resultado.
+
+// Ejemplo: var arrayNum1 = [1, 23, 34];
+// La función devolvería 34-1 = 33
+
+let arrayNum1 = [12, 23, 34, 567, 19, 57];
+
+function arrayCheck(arrayNum1) {
+  let valueMax = arrayNum1[0];
+  let valueMin = arrayNum1[0];
+  for (let index = 0; index < arrayNum1.length; index++) {
+    valueMax = (arrayNum1[index] > valueMax) ? arrayNum1[index] : valueMax
+    valueMin = (arrayNum1[index] < valueMin) ? arrayNum1[index] : valueMin
+  }
+
+  return valueMax - valueMin
+}
+
+console.log("La diferencia entre el valor más alto y el más bajod el array es " + arrayCheck(arrayNum1))
---
+++ arrays/ejercicio9.js
@@ -0,0 +1,19 @@
+// 9. Dado un array, declarar una función que recoja el array como argumento y lo ordene de menor a mayor.
+// Ejecutar la función y mostrar el array ordenado. 
+// Nota: No se puede utilizar la función SORT de JAVASCRIPT.
+
+let arrayNum1 = [12, 23, 34, 567, 19, 57];
+
+function arrayOrder(arrayNum1) {
+  for (let i = 0; i < arrayNum1.length - 1; i++) {
+    for (let j = i + 1; j < arrayNum1.length; j++)
+      if (arrayNum1[i] > arrayNum1[j]) {
+        const aux = arrayNum1[i];
+        arrayNum1[i] = arrayNum1[j]
+        arrayNum1[j] = aux
+      }
+  }
+  return console.log(arrayNum1);
+}
+
+arrayOrder(arrayNum1)
