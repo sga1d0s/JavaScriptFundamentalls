3005d73ea0b8c0dc5e412b98f645a4ab4cba8fd2
sga1d0s <galdos.sergio@outlook.com>
2024-11-29 07:06:01 +0100

    [dev][ejer23] finish

---
+++ ejer23/game.js
@@ -0,0 +1,33 @@
+import { Mage } from './Mage.js';
+import { Spell } from './Spell.js';
+import { combat, showAttr } from './combat.js';
+
+// crear hechizos
+const lightning = new Spell("Lightning", 24, 12);
+const fireball = new Spell("Fireball", 35, 15);
+
+// crear mago1
+const mage1 = new Mage("Eldritch", 5, 200, 150, 12);
+mage1.spell = lightning;
+
+// crear mago2
+const mage2 = new Mage("Asseth", 3, 300, 170, 12);
+mage2.spell = fireball;
+
+// mostrar atributos
+showAttr(mage1);
+showAttr(mage2);
+
+// combate: mage1 ataca a mage2
+combat(mage1, mage2);
+
+// mostrar atributos prmer ataque
+showAttr(mage1);
+showAttr(mage2);
+
+// combate: mage2 ataca a mage1
+combat(mage2, mage1);
+
+// mostrar atributos segundo ataque
+showAttr(mage1);
+showAttr(mage2);
