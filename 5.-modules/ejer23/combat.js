bb7b326a3aa9ea0e525dd0d01748ed88cad9f2eb
Sergio Galdos <galdos.sergio@outlook.com>
2025-03-18 06:27:31 +0100

    update ejer 23

--- 5.-modules/ejer23/combat.js
+++ 5.-modules/ejer23/combat.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:ejer23/combat.js
 // métodos para manejar combates y mostrar atributos de mago
 
 // función combat
@@ -30,29 +29,4 @@ export function showAttr(mage) {
     console.log(`\n  Daño del Hechizo: ${mage.spell.damage}`);
     console.log(`  Coste de Maná: ${mage.spell.points}`);
   }
-=======
-// métodos para manejar combates y mostrar atributos de mago
-
-// función combat
-export function combat(attacker, defender) {
-
-  // calcular el daño
-  const damage = attacker.level * attacker.strength + attacker.spell.damage;
-
-  // ceducir la vida del defensor
-  defender.life -= damage;
-
-  // reducir el maná del atacante
-  attacker.mana -= attacker.spell.points;
-}
-
-// función para mostrar resultados
-export function showAttr(mage) {
-  console.log(`\n----------- Atributos de ${mage.name}`);
-  console.log(`Nivel: ${mage.level}`);
-  console.log(`Vida: ${mage.life}`);
-  console.log(`Maná: ${mage.mana}`);
-  console.log(`Fuerza: ${mage.strength}`);
-  console.log(`Hechizo: ${mage.spell ? mage.spell.name : "Ninguno"}`);
->>>>>>> refs/remotes/origin/develop:5.-modules/ejer23/combat.js
 }
