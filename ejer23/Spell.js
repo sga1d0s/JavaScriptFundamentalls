f1c4c2671c50f529f5f2941560e4ee9110710924
Sergio Galdos <galdos.sergio@outlook.com>
2025-03-26 20:05:57 +0100

    [dev] update ejercices

--- ejer23/Spell.js
+++
@@ -1,8 +0,0 @@
-// clase 'Spell' como plantilla para cerar echizos
-export class Spell {
-  constructor(name, damage, points) {
-    this.name = name;
-    this.damage = damage;
-    this.points = points;
-  }
-}
