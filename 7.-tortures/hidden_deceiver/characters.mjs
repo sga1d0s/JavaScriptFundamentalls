9c12e948663c50021edd25e8cc74578aaefe01e7
Sergio Galdos <galdos.sergio@outlook.com>
2025-06-20 01:02:49 +0200

    [dev] ADD torture - hidden deceiver

---
+++ 7.-tortures/hidden_deceiver/characters.mjs
@@ -0,0 +1,87 @@
+export default [
+  {
+    name: "Ragnar Wolfbane",
+    level: 32,
+    age: 45,
+    stamina: 87,
+    weapon: {
+      name: "Stormbreaker Axe",
+      minLevel: 30,
+      damage: 25,
+      durability: 80,
+    },
+    armor: {
+      name: "Shadowplate",
+      minLevel: 28,
+      defense: 15,
+    }
+  },
+  {
+    name: "Seraphina Nightshade",
+    level: 27,
+    age: 120,
+    stamina: 92,
+    weapon: {
+      name: "Celestial Rapier",
+      minLevel: 25,
+      damage: 18,
+      durability: 75,
+    },
+    armor: {
+      name: "Phantom Shroud",
+      minLevel: 22,
+      defense: 12,
+    }
+  },
+  {
+    name: "Thalgrim Ironfist",
+    level: 40,
+    age: 52,
+    stamina: 95,
+    weapon: {
+      name: "Obsidian Warhammer",
+      minLevel: 38,
+      damage: 30,
+      durability: 90,
+    },
+    armor: {
+      name: "Titan's Bulwark",
+      minLevel: 35,
+      defense: 18,
+    }
+  },
+  {
+    name: "Lyara Moonwhisper",
+    level: 35,
+    age: 29,
+    stamina: 88,
+    weapon: {
+      name: "Starfire Bow",
+      minLevel: 33,
+      damage: 22,
+      durability: 85,
+    },
+    armor: {
+      name: "Sylvan Guardian",
+      minLevel: 30,
+      defense: 14,
+    }
+  },
+  {
+    name: "Draven Blackthorn",
+    level: 30,
+    age: 37,
+    stamina: 90,
+    weapon: {
+      name: "Doomfang Dagger",
+      minLevel: 28,
+      damage: 20,
+      durability: 78,
+    },
+    armor: {
+      name: "Abyssal Carapace",
+      minLevel: 25,
+      defense: 10,
+    }
+  },
+]
---
+++ 7.-tortures/hidden_deceiver/index.mjs
@@ -0,0 +1,21 @@
+
+
+main()
+
+function main() {
+
+  // crear todos los objetos e introducirlos en arrays
+
+  // mostrar listado de todas las armas y armaduras
+
+  // mostrar todos los persojnajes con sus atributos y equipamiento
+
+  // seleccionar armaduras con nivel menor de 30 mostrar nombre y nivel
+
+  // buscar para cada personaje todas las armas que pueden utilizar segun nivel
+  // y añadirlas a su equipamiento. Mostrar nombre y nivel de cada personaje junto a
+  //nombre y nivel de cada arma que lleve
+
+  // ejecutar método fumble para un personaje y mostrar sus datos actualizados
+
+}
